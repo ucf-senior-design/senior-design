312d280b2d1eb4b3843a305180f9496c15aec7df
Jara <araj_mont@yahoo.com>
2022-12-12 15:13:18 -0500

    Modified app/index to include navbar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,31 +1,34 @@
-import * as React from 'react';
-import type { AppProps } from 'next/app';
 import { CacheProvider, EmotionCache } from '@emotion/react';
-import { ThemeProvider, CssBaseline, createTheme } from '@mui/material';
+import { createTheme, ThemeProvider } from '@mui/material';
+import CssBaseline from '@mui/material/CssBaseline';
+import type { AppProps } from 'next/app';
+import * as React from 'react';
+import Navbar from './components/navbar';
 
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
-import createEmotionCache from '../utility/createEmotionCache';
-import lightThemeOptions from '../styles/theme/lightThemeOptions';
 import '../styles/globals.css';
+import theme from '../styles/theme/theme';
+import createEmotionCache from '../utility/createEmotionCache';
 interface MyAppProps extends AppProps {
   emotionCache?: EmotionCache;
 }
 
 const clientSideEmotionCache = createEmotionCache();
 
-const lightTheme = createTheme(lightThemeOptions);
+const defaultTheme = createTheme(theme);
 
 const MyApp: React.FunctionComponent<MyAppProps> = (props) => {
   const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
 
   return (
     <CacheProvider value={emotionCache}>
-      <ThemeProvider theme={lightTheme}>
+      <ThemeProvider theme={defaultTheme}>
         <CssBaseline />
+        <Navbar />
         <Component {...pageProps} />
       </ThemeProvider>
     </CacheProvider>
