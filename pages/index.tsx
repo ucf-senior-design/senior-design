48f32126b947a720674b044ce407dd3e4b109993
Jara <araj_mont@yahoo.com>
2022-12-20 02:34:46 -0500

    added people graphic, might revamp later? idk

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import { Grid, Typography } from '@mui/material';
 import { useTheme } from "@mui/material/styles";
 import type { NextPage } from 'next';
 import Head from 'next/head';
+import BlueFooter from '../src/assets/blueFooter';
 import Cloud from '../src/assets/cloud';
 import styles from '../styles/Home.module.css';
 import SignUpButton from './components/SignUpButton';
@@ -21,7 +22,9 @@ const Home: NextPage = () => {
             <Typography variant='h2' style={{fontWeight:500, color:theme.palette.lace.main}} sx={{mb:2}}>welcome to your personal trip planner.</Typography>
             <SignUpButton />
         </Grid>
+        <img src='./people.png' alt="Group of People" className={styles.people} />
         <Cloud />
+        <BlueFooter />
       </main>
     </div>
   );
---
+++ src/assets/blueFooter.tsx
@@ -0,0 +1,11 @@
+
+
+export const BlueFooter = () => {
+    return (
+        <svg width="100vw" height="98" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <rect width="100vw" height="98" fill="#1746A2" />
+        </svg>
+    )
+}
+
+export default BlueFooter;
