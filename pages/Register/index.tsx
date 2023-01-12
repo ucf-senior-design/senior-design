60b62855a203bac6f7d10eb70dd002e789bf804a
Jonathan Dieu <jonathanhuydieu@gmail.com>
2023-01-12 08:28:47 -0900

    Register WIP

---
+++ pages/Register/index.tsx
@@ -0,0 +1,12 @@
+import { RegisterForm } from '../../components/RegisterComponents/RegisterForm';
+
+const label = { inputProps: { 'aria-label': 'Switch demo' } };
+
+export default function Login() {
+  return (
+    // This corresponds to the container class in the /styles folder
+    <div>
+      <RegisterForm />
+    </div>
+  );
+}
