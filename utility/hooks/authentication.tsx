68304472514fbcd5a9892e7180fb6d49212a799e
Victoria Williamson <vicwill01@knights.ucf.edu>
2023-01-21 17:26:43 -0500

    fix if statement

--- utility/hooks/authentication.tsx
+++ utility/hooks/authentication.tsx
@@ -1,7 +1,7 @@
 import {
   FacebookAuthProvider,
   GoogleAuthProvider,
-  signInWithPopup
+  signInWithPopup,
 } from 'firebase/auth';
 import Router from 'next/router';
 import React from 'react';
@@ -233,7 +233,7 @@ export function AuthProvider({ children }: { children: React.ReactNode }) {
       JSON.stringify({
         email: user ? user.email : '',
         uid: user ? user.uid : '',
-        purpose: 'email'
+        purpose: 'email',
       }),
       'POST'
     );
@@ -243,8 +243,9 @@ export function AuthProvider({ children }: { children: React.ReactNode }) {
       if (response.status === EMAIL_VERIFIED) {
         // TODO: Handle accounts That have a verified email already
         console.log('Email Already Verified');
+      } else {
+        callback({ isSuccess: response.ok });
       }
-      callback({ isSuccess: response.ok });
     } else {
       callback({ isSuccess: response.ok, errorMessage: await response.text() });
     }
@@ -260,7 +261,7 @@ export function AuthProvider({ children }: { children: React.ReactNode }) {
     if (response.ok) {
       await storePartialCredentialResult(await response.json());
       // TODO: Create Details Page
-      Router.push('/Auth/RegisterEmail')
+      Router.push('/Auth/RegisterEmail');
     } else {
       callback({ isSuccess: response.ok, errorMessage: await response.text() });
     }
@@ -281,7 +282,7 @@ export function AuthProvider({ children }: { children: React.ReactNode }) {
         await saveRegisterdUser(await response.json());
       } else if (response.status === MUST_VERIFY_EMAIL) {
         // Go to Email Verficications Pge
-        Router.push('/Auth/RegisterEmail')
+        Router.push('/Auth/RegisterEmail');
       } else if (response.status === MUST_ADD_DETAILS) {
         await storePartialCredentialResult(await response.json());
         // Go to Details Page
