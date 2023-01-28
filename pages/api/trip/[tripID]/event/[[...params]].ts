4c1f0bed4e18a7ba35cba3a7b3e4b4b947c47f75
Victoria Williamson <vicwill01@knights.ucf.edu>
2023-01-27 23:51:04 -0500

    cleaning api

--- pages/api/trip/[tripID]/event/[[...params]].ts
+++ pages/api/trip/[tripID]/event/[[...params]].ts
@@ -11,7 +11,6 @@ export default async function handler(
 
   switch (req.method) {
     case 'POST': {
-      console.log(req.body);
       await firebaseAdmin
         .firestore()
         .collection(`Trips/${tripID}/events`)
