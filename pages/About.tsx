7399bd44be26c85e8efe1f7e8cbd65c4c2b6cf0b
Victoria Williamson <vicwill01@knights.ucf.edu>
2023-02-02 02:06:02 -0500

    add back moutnains

--- pages/About.tsx
+++ pages/About.tsx
@@ -1,11 +1,13 @@
 import MenuBookIcon from '@mui/icons-material/MenuBook';
 import PersonAddAlt1Icon from '@mui/icons-material/PersonAddAlt1';
 import PublicIcon from '@mui/icons-material/Public';
+import Image from 'next/image';
 import {
   Box,
   Card,
   Container,
   Grid,
+  Stack,
   Typography,
   useTheme,
 } from '@mui/material';
@@ -37,63 +39,93 @@ export default function About() {
   const boxItems = [item1, item2, item3];
 
   return (
-    <div>
-      <Grid container>
-        <Grid container xs={8} sx={{ m: 3, mx: 5 }}>
-          <Typography
-            variant="h2"
-            style={{ fontWeight: 500, color: theme.palette.highlight.main }}
-          >
-            a collaborative approach to group planning.
+    <>
+      <div
+        style={{
+          position: 'relative',
+          zIndex: 5,
+          height: '100%',
+          width: '100%',
+          alignItems: 'center',
+          justifyContent: 'center',
+          flexDirection: 'column',
+          display: 'flex',
+        }}
+      >
+        <Box
+          display="flex"
+          flexDirection={'column'}
+          sx={{ textAlign: 'center' }}
+        >
+          <Typography variant="h2" color="white" style={{ fontWeight: 500 }}>
+            a collaborative approach to group planning
           </Typography>
-          <Typography
-            variant="h4"
-            style={{ fontWeight: 300, color: theme.palette.highlight.main }}
-          >
+          <Typography variant="h4" color="white" style={{ fontWeight: 300 }}>
             designed for quick and painless planning, even with lots of moving
             parts
           </Typography>
-        </Grid>
 
-        <Box
-          display="flex"
-          flexDirection={{ xs: 'column', sm: 'row' }}
-          sx={{ margin: 'auto' }}
-        >
-          {boxItems.map((item) => (
-            <Grid
-              container
-              key={item.id}
-              xs={8}
-              direction="column"
-              sx={{ justify: 'center', alignItems: 'center', margin: 8 }}
-            >
-              <item.icon style={{ fontSize: '500%' }} />
-              <Card
-                sx={{
-                  backgroundColor: theme.palette.secondary.main,
-                  minHeight: 230,
-                  maxHeight: 500,
-                }}
+          <Box
+            display="flex"
+            flexDirection={{ xs: 'column', sm: 'row' }}
+            sx={{ margin: 'auto' }}
+          >
+            {boxItems.map((item) => (
+              <Grid
+                container
+                key={item.id}
+                xs={8}
+                direction="column"
+                flexWrap={'wrap'}
+                sx={{ justify: 'center', alignItems: 'center', margin: 8 }}
               >
-                <Container>
-                  <Grid
-                    container
-                    padding={7}
-                    direction="column"
-                    sx={{ justify: 'center', alignItems: 'center' }}
-                  >
-                    <Typography variant="h4" marginBottom={1} color="white">
-                      {item.header}
-                    </Typography>
-                    <Typography color="white">{item.subtitle}</Typography>
-                  </Grid>
-                </Container>
-              </Card>
-            </Grid>
-          ))}
+                <Card
+                  sx={{
+                    backgroundColor: 'rgba(14, 14, 20, 0.64)',
+                    height: '300px',
+                    width: '400px',
+                  }}
+                >
+                  <Container>
+                    <Grid
+                      container
+                      padding={7}
+                      direction="column"
+                      sx={{ justify: 'center', alignItems: 'center' }}
+                    >
+                      <Typography
+                        variant="h4"
+                        marginBottom={1}
+                        color="white"
+                        sx={{ fontWeight: 'bold' }}
+                      >
+                        {item.header}
+                      </Typography>
+                      <Typography color="white">{item.subtitle}</Typography>
+                    </Grid>
+                  </Container>
+                </Card>
+              </Grid>
+            ))}
+          </Box>
         </Box>
-      </Grid>
-    </div>
+      </div>
+      <Stack
+        style={{
+          height: '100%',
+          width: '100%',
+          display: 'absolute',
+          zIndex: 1,
+          overflow: 'hidden',
+        }}
+      >
+        <Image
+          src="/mountains.svg"
+          alt="Landing"
+          layout={'fill'}
+          objectFit={'cover'}
+        />
+      </Stack>
+    </>
   );
 }
