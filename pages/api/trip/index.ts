cdfb1bd80e686fbd83b311b39d3d036ab221f26a
Jonathan Dieu <jonathanhuydieu@gmail.com>
2023-02-02 20:26:15 -0900

    Trip endpoint spaghetti code wip

--- pages/api/trip/index.ts
+++ pages/api/trip/index.ts
@@ -1,8 +1,57 @@
 import { NextApiRequest, NextApiResponse } from 'next';
+import firebaseAdmin from '../../../utility/firebaseAdmin';
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  res.status(200).send('Hello Trips.');
+  switch (req.method) {
+    case 'POST': {
+      console.log(req.body);
+      await firebaseAdmin.firestore().collection('Trips/').add(req.body);
+      // .then(async (value) => {
+      //   const id = (await value.get()).id;
+      //   const data = (await value.get()).data();
+      //   res.status(200).send({ uid: id, ...data });
+      // })
+      // .catch(() => {
+      //   res.status(400).send('Could not create event.');
+      // });
+      res.status(200).send({});
+      break;
+    }
+    case 'GET': {
+      let trips = new Map<string, any>();
+      const {
+        query: { user },
+      } = req;
+      try {
+        let allTrips = await firebaseAdmin
+          .firestore()
+          .collection('Trips/')
+          .get();
+        allTrips.forEach((doc) => {
+          if (
+            Object.hasOwn(doc.data(), 'attendees') &&
+            doc.data()['attendees'].includes(user)
+          ) {
+            trips.set(doc.id, doc.data());
+          }
+        });
+      } catch (e) {
+        res
+          .status(400)
+          .send(
+            'Error when loading trips'
+          );
+      }
+      res.status(200).send(Object.fromEntries(trips));
+      break;
+    }
+
+    // case 'DELETE': {
+    // }
+  }
+  const tripID = req.query.tripID as string;
+  const params = req.query.params;
 }
