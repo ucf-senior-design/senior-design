4a1225c4c1dd622c6409ded4e962e38d2e268393
Victoria Williamson <vicwill01@knights.ucf.edu>
2023-02-15 16:29:38 -0500

    modified post and get trip

--- pages/api/trip/index.ts
+++ pages/api/trip/index.ts
@@ -1,5 +1,5 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import { firebaseAuth } from '../../../utility/firebase';
+import { firebaseAuth, unpackArrayResponse } from '../../../utility/firebase';
 import firebaseAdmin from '../../../utility/firebaseAdmin';
 
 export default async function handler(
@@ -8,48 +8,38 @@ export default async function handler(
 ) {
   switch (req.method) {
     case 'POST': {
-      console.log(req.body);
-      try {
-      await firebaseAdmin.firestore().collection('Trips/').add(req.body);
-      } catch (e) {
-        res.status(400).send('Error when creating trip.')
-      }
-      res.status(200).send({});
+      await firebaseAdmin
+        .firestore()
+        .collection('Trips/')
+        .add(req.body)
+        .then((value) => {
+          res.status(200).send({ uid: value.id, ...req.body });
+        })
+        .catch((e) => {
+          res.status(400).send('Cannot create Trip.');
+        });
+
       break;
     }
     case 'GET': {
       if (firebaseAuth.currentUser == null) {
-        res.status(400).send('User is not logged in.')
+        res.status(400).send('User is not logged in.');
+        break;
       }
-      
-      let trips = new Map<string, any>();
-      try {
-        let allTrips = await firebaseAdmin
-          .firestore()
-          .collection('Trips/')
-          .get();
-        allTrips.forEach((doc) => {
-          if (
-            Object.hasOwn(doc.data(), 'attendees') && firebaseAuth.currentUser != null &&
-            doc.data()['attendees'].includes(firebaseAuth.currentUser.uid)
-          ) {
-            trips.set(doc.id, doc.data());
-          }
+      await firebaseAdmin
+        .firestore()
+        .collection(`Trips`)
+        .where('attendees', 'array-contains', firebaseAuth.currentUser.uid)
+        .orderBy('duration.start')
+        .get()
+        .then((value) => {
+          const trips = unpackArrayResponse(value.docs);
+          res.status(200).send(trips);
+        })
+        .catch((e) => {
+          res.status(400).send('Error when loading trips');
         });
-      } catch (e) {
-        res
-          .status(400)
-          .send(
-            'Error when loading trips'
-          );
-      }
-      res.status(200).send(Object.fromEntries(trips));
       break;
     }
-
-    // case 'DELETE': {
-    // }
   }
-  const tripID = req.query.tripID as string;
-  const params = req.query.params;
 }
