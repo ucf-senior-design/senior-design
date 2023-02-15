65c1b420de38feb1ce41727b1008971c40647aec
Victoria Williamson <vicwill01@knights.ucf.edu>
2023-02-15 16:52:06 -0500

    more API update

--- pages/api/trip/[tripID]/[[...params]].ts
+++ pages/api/trip/[tripID]/[[...params]].ts
@@ -1,66 +1,70 @@
-import { arrayRemove, arrayUnion, doc, updateDoc } from "firebase/firestore";
+import { arrayRemove, arrayUnion, doc, updateDoc } from 'firebase/firestore';
 import { NextApiRequest, NextApiResponse } from 'next';
-import { firebaseDatbase } from "../../../../utility/firebase";
-import firebaseAdmin from "../../../../utility/firebaseAdmin";
-
+import { firebaseAuth, firebaseDatbase } from '../../../../utility/firebase';
+import firebaseAdmin from '../../../../utility/firebaseAdmin';
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-    const tripID = req.query.tripID as string;
-    const params = req.query.params;
-    switch (req.method) {
-        case 'DELETE': {
-        if (tripID == undefined || tripID.length == 0) {
-            res.status(400).send('tripID is undefined')
-        } else {
+  const tripID = req.query.tripID as string;
+  const params = req.query.params;
+  switch (req.method) {
+    case 'DELETE': {
+      if (tripID == undefined || tripID.length == 0) {
+        res.status(400).send('tripID is undefined');
+      } else {
         firebaseAdmin
-            .firestore()
-            .collection(`Trips/`)
-            .doc(tripID)
-            .delete()
-            .then(() => {
+          .firestore()
+          .collection(`Trips/`)
+          .doc(tripID)
+          .delete()
+          .then(() => {
             res.status(200).send({});
-            })
-            .catch((e: any) => {
-            console.log(e)
+          })
+          .catch((e: any) => {
+            console.log(e);
             res.status(400).send('Could not delete trip.');
-            });
-        }
-        break;
+          });
+      }
+      break;
     }
 
     case 'PUT': {
-        const purpose = req.body.purpose
-        if (purpose != 'join' && purpose != 'leave') {
-        res.status(400).send('Invalid purpose')
-        } else {
+      if (
+        params === undefined ||
+        firebaseAuth.currentUser === null ||
+        (params[0] !== 'join' && params[0] !== 'leave')
+      ) {
+        res.status(400).send('Invalid purpose');
+        return;
+      } else {
+        const purpose = params[0];
+        const userID = firebaseAuth.currentUser?.uid;
 
-        const userID = req.body.userID; 
         try {
-            const docRef = doc(firebaseDatbase, "Trips", tripID);
-            switch (purpose) {
+          const docRef = doc(firebaseDatbase, 'Trips', tripID);
+          switch (purpose) {
             case 'join': {
-                await updateDoc(docRef, {
-                attendees: arrayUnion(userID)
-                });
-                break
+              await updateDoc(docRef, {
+                attendees: arrayUnion(userID),
+              });
+              break;
             }
             case 'leave': {
-                await updateDoc(docRef, {
-                attendees: arrayRemove(userID)
-                });
-                break
+              await updateDoc(docRef, {
+                attendees: arrayRemove(userID),
+              });
+              break;
             }
-            }
-            res.status(200).send(docRef.id);
+          }
+          res.status(200).send('Success.');
         } catch (e: any) {
-            res.status(400).send('Error when updating trip.');
+          console.log(e);
+          res.status(400).send('Error when updating trip.');
         }
-        break;  
-        }
+        break;
+      }
     }
-        
-    }
+  }
 }
