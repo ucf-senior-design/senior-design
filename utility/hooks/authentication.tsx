f5d453cb953da2c893939f8a45cf740046f23ab2
Victoria Williamson <vicwill01@knights.ucf.edu>
2023-02-15 16:55:33 -0500

    fix going to wrong page

--- utility/hooks/authentication.tsx
+++ utility/hooks/authentication.tsx
@@ -1,7 +1,7 @@
 import {
   FacebookAuthProvider,
   GoogleAuthProvider,
-  signInWithPopup
+  signInWithPopup,
 } from 'firebase/auth';
 import Router from 'next/router';
 import React from 'react';
@@ -233,7 +233,7 @@ export function AuthProvider({ children }: { children: React.ReactNode }) {
       JSON.stringify({
         email: user ? user.email : '',
         uid: user ? user.uid : '',
-        purpose: 'email'
+        purpose: 'email',
       }),
       'POST'
     );
@@ -260,7 +260,7 @@ export function AuthProvider({ children }: { children: React.ReactNode }) {
     if (response.ok) {
       await storePartialCredentialResult(await response.json());
       // TODO: Create Details Page
-      Router.push('/Auth/RegisterEmail')
+      Router.push('/Auth/RegisterEmail');
     } else {
       callback({ isSuccess: response.ok, errorMessage: await response.text() });
     }
@@ -281,13 +281,11 @@ export function AuthProvider({ children }: { children: React.ReactNode }) {
         await saveRegisterdUser(await response.json());
       } else if (response.status === MUST_VERIFY_EMAIL) {
         // Go to Email Verficications Pge
-        Router.push('/Auth/RegisterEmail')
+        Router.push('/Auth/RegisterEmail');
       } else if (response.status === MUST_ADD_DETAILS) {
         await storePartialCredentialResult(await response.json());
-        // Go to Details Page
-        // Router.push('/Auth/Details');
-        // Go to the Dashboard Page
-        Router.push('/Dashboard');
+        //Go to Details Page
+        Router.push('/Auth/Details');
       }
       return;
     }
@@ -310,6 +308,4 @@ export function AuthProvider({ children }: { children: React.ReactNode }) {
       callback({ isSuccess: response.ok, errorMessage: await response.text() });
     }
   }
-
-
 }
