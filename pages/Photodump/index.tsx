b08ae496745a999388b971aa966f174663819b07
Jonathan Dieu <jonathanhuydieu@gmail.com>
2023-02-18 09:06:38 -0900

    Make the deployment stop failing from react optimizations

--- pages/Photodump/index.tsx
+++ pages/Photodump/index.tsx
@@ -4,8 +4,8 @@ import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import { Box } from '@mui/system';
 import Gallery from 'react-photo-gallery';
-import { samplephotos } from './samplephotos';
-import { samplephotos2 } from './samplephotos2';
+import { samplephotos } from '../../src/samplephotos';
+import { samplephotos2 } from '../../src/samplephotos2';
 
 export default function Photodump() {
   const BasicRows = () => <Gallery photos={samplephotos2} />;
@@ -34,10 +34,10 @@ export default function Photodump() {
           <input hidden accept="image/*" type="file" />
           <PhotoCamera />
         </IconButton>
-      <Button variant="contained" component="label" size="large">
-        Download All
-        <input hidden accept="image/*" multiple type="file" />
-      </Button>
+        <Button variant="contained" component="label" size="large">
+          Download All
+          <input hidden accept="image/*" multiple type="file" />
+        </Button>
       </Box>
     </>
   );
