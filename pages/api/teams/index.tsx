5aeea1a88dce8d1effe5df83b0c79cfe7e7595ae
jaeivan <jaeivanromero@gmail.com>
2023-02-21 18:27:10 -0500

    Address last comment

--- pages/api/teams/index.tsx
+++ pages/api/teams/index.tsx
@@ -9,7 +9,7 @@ export default async function handler(
     case 'POST': { // assume req.body is a valid team
     try {
         let trip = await firebaseAdmin.firestore().collection('Teams/').add(req.body);
-        res.status(200).send(trip.id);
+        res.status(200).send((await trip.get()).data());
     } catch (e) {
         res.status(400).send('Error when creating team.')
     }
