d34a4dcde9106222a1777bda09c7d58ce1756e34
jaeivan <jaeivanromero@gmail.com>
2023-02-22 15:23:58 -0500

    Return uid when creating a team

--- pages/api/teams/index.tsx
+++ pages/api/teams/index.tsx
@@ -9,7 +9,7 @@ export default async function handler(
     case 'POST': { // assume req.body is a valid team
     try {
         let trip = await firebaseAdmin.firestore().collection('Teams/').add(req.body);
-        res.status(200).send((await trip.get()).data());
+        res.status(200).send({uid: trip.id, team: (await trip.get()).data()});
     } catch (e) {
         res.status(400).send('Error when creating team.')
     }
