5e47d08f49f69e25733079bd14bd6e4240d4c064
farnsworthe <farnsworthe@knights.ucf.edu>
2023-03-01 05:06:59 -0500

    team ui added for view and edit pages

---
+++ pages/Create_Team.tsx
@@ -0,0 +1,36 @@
+import { Grid, Stack } from '@mui/material';
+import { TeamCreation } from '../components/Dashboard/Widgets/TeamComponents/TeamCreationForm';
+
+
+export default function Teams() {
+    return (
+        <div style={$wrapper}>
+          <Grid
+            container
+            direction="column"
+            justifyContent="center"
+            alignItems="center"
+          >
+            <TeamCreation />
+            <Stack
+              sx={{ display: { xs: 'none', md: 'block' } }}
+              style={{
+                height: '30%',
+                width: '30%',
+                display: 'absolute',
+                zIndex: 1,
+              }}
+            >
+            </Stack>
+          </Grid>
+        </div>
+      );
+    }
+    
+    const $wrapper: React.CSSProperties = {
+      width: '100%',
+      height: '100%',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+    };
