f6a46b07257e717179356e0dad1989c03946b0a5
farnsworthe <farnsworthe@knights.ucf.edu>
2023-03-01 18:43:06 -0500

    teams ui changes

--- components/Dashboard/Widgets/TeamComponents/TeamForm.tsx
+++ components/Dashboard/Widgets/TeamComponents/TeamForm.tsx
@@ -4,7 +4,6 @@ import theme from '../../../../styles/theme/Theme';
 import Avatar from '../../../Avatar';
 import UserSearch from '../../../Form/UserSearch';
 
-
   export const TeamForm = (
     {purpose}:
     {purpose: 'create' | 'view'}
@@ -18,8 +17,6 @@ import UserSearch from '../../../Form/UserSearch';
       console.info('You clicked the delete icon.');
     };
 
-
-  
     return (
       <Paper
         elevation={3}
@@ -52,102 +49,98 @@ import UserSearch from '../../../Form/UserSearch';
           >
             {teamname}
           </Typography>
-
-          
+     
           <Typography
-              style={{ color: theme.palette.secondary.main, fontSize:16, paddingBottom: 5, fontWeight:500, textAlign:"left", width:"100%"}}
-              >
-                  name
-              </Typography>
+            style={{ color: theme.palette.secondary.main, fontSize:16, paddingBottom: 5, fontWeight:500, textAlign:"left", width:"100%"}}
+          >
+            name
+          </Typography>
+
           <TextField
-                  color="secondary"
-                  size="small"
-                  label={teamname}
-                  sx={{ flexGrow: 1 }}
-                  fullWidth
-                  />
+            color="secondary"
+            size="small"
+            label={teamname}
+            sx={{ flexGrow: 1 }}
+            fullWidth
+          />
           <br />
           
-
           <Typography
           style={{ color: theme.palette.secondary.main, fontSize:16, paddingBottom: 5, textAlign:"left", width:"100%", fontWeight:500}}
           >
-              members
+            members
           </Typography>
-              <Stack direction="row" spacing={1}>
-                {example.map((item) => (
-                  <Chip variant="outlined" icon={<Avatar name={item.username} size={25}/>} label={<Typography padding={1} fontSize={14}> {item.name} </Typography>} 
-                  onDelete={handleDelete} size="medium" sx={{paddingTop:1.5, paddingBottom:1.5, paddingLeft:.5, paddingRight:.5}}/>))}
-              </Stack>
-              <br /><br />
-              
-              
 
-              
-              <Typography
-              style={{ color: theme.palette.secondary.main, fontSize:16, fontWeight:500, paddingBottom: 15}}
-              >
-                  add a friend
-              </Typography>
-              
-              <UserSearch
-              sx={{ width: '100%', marginBottom: '10px' }}
-              handleFoundUser={(user) =>
-                console.log(user)
-              }
-              />
-              
-              <Divider sx={{width:"100%", color:"#868686", borderBottomWidth:"30"}}>
-              <Typography>OR</Typography>
-              </Divider>
+          <Stack direction="row" spacing={1}>
+            {example.map((item) => (
+              <Chip key={item.id} variant="outlined" icon={<Avatar name={item.username} size={25}/>} label={
+              <Typography padding={1} fontSize={14}> {item.name} </Typography>} 
+              onDelete={handleDelete} size="medium" sx={{paddingTop:1.5, paddingBottom:1.5, paddingLeft:.5, paddingRight:.5}}/>))}
+          </Stack>
 
+          <br /><br />
+             
+          <Typography
+          style={{ color: theme.palette.secondary.main, fontSize:16, fontWeight:500, paddingBottom: 15}}
+          >
+            add a friend
+          </Typography>
               
-              <Typography
-              style={{ color: theme.palette.secondary.main, fontSize:16, paddingBottom: 15, fontWeight:500}}
-              >
-                  add by username:
-              </Typography>
+          <UserSearch
+          sx={{ width: '100%', marginBottom: '10px' }}
+          handleFoundUser={(user) =>
+            console.log(user)}
+          />
               
+          <Divider sx={{width:"100%", color:"#868686", borderBottomWidth:"30"}}>
+            <Typography>OR</Typography>
+          </Divider>
+              
+          <Typography
+          style={{ color: theme.palette.secondary.main, fontSize:16, paddingBottom: 15, fontWeight:500}}
+          >
+            add by username:
+          </Typography>
 
-              <UserSearch
-              sx={{ width: '100%', marginBottom: '10px' }}
-              handleFoundUser={(user) =>
-                console.log(user)
-              }
-              />
+          <UserSearch
+          sx={{ width: '100%', marginBottom: '10px' }}
+          handleFoundUser={(user) =>
+            console.log(user)}
+          />
               
-              {purpose === 'create' && 
-              <Button
-              variant="contained"
-              color="primary"
-              size="medium"
-              sx={{ borderRadius: 1, width:"100%", fontSize:16 }}
-              aria-label="create team"
-              > 
-                create team
-              </Button>}
+          {purpose === 'create' && 
+          <Button
+          variant="contained"
+          color="primary"
+          size="medium"
+          sx={{ borderRadius: 1, width:"100%", fontSize:16 }}
+          aria-label="create team"
+          > 
+            create team
+          </Button>}
 
-              {purpose === "view" && <>
-              <Button
-              variant="contained"
-              color="primary"
-              size="medium"
-              sx={{ borderRadius: 1, width:"100%", fontSize:16 }}
-              aria-label="save changes"
-              > 
-                save changes
-              </Button>
-              <Button
-              variant="contained"
-              color="error"
-              size="medium"
-              sx={{ borderRadius: 1, width:"100%", fontSize:16 }}
-              aria-label="delete team"
-              > 
-                delete team
-              </Button>
-              </>
-              }
+          {purpose === "view" && <>
+          <Button
+          variant="contained"
+          color="primary"
+          size="medium"
+          sx={{ borderRadius: 1, width:"100%", fontSize:16 }}
+          aria-label="save changes"
+          > 
+            save changes
+          </Button>
+          
+          <Button
+          variant="contained"
+          color="error"
+          size="medium"
+          sx={{ borderRadius: 1, width:"100%", fontSize:16 }}
+          aria-label="delete team"
+          > 
+            delete team
+          </Button>
+          </>
+          }
 
             </Box>
 
