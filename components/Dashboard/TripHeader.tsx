ed88731a9a20e98f1a42f777e54c8d0f9e4d56cb
farnsworthe <farnsworthe@knights.ucf.edu>
2023-03-06 18:29:22 -0500

    need to merge

---
+++ components/Dashboard/TripHeader.tsx
@@ -0,0 +1,46 @@
+
+export const TripHeader = (props:any) => {
+    const trip_details = {location:'Orlando', date_start: 'October 1', date_end: 'October 3', participants:[['hajime', 'username'], ['noriyuki', 'username2'], ['minoru', 'username3']]}
+    
+    return <></>}
+//         <Grid
+//         container
+//         style={{
+//         backgroundImage: '`url(require("../../public/header.svg"))`',
+//         padding: '20px',
+//         }}
+//     >
+//         <Grid
+//         item
+//         xs={4}
+//         sx={{
+//             display: 'flex',
+//             flexDirection: 'row',
+//             gap: 1,
+//             alignItems: 'center',
+//             justifyContent: 'start',
+//         }}
+//         >
+//         <Typography
+//             sx={{ color: 'white', fontWeight: '700', fontSize: '40px' }}
+//         >
+//             {trip_details.location}
+//         </Typography>
+
+//         <Typography
+//             sx={{ color: 'white', fontWeight: '400', fontSize: '20px' }}
+//         >
+//             {trip_details.date_start} - {trip_details.date_end}
+//         </Typography>
+
+//         {trip_details.participants.map((item) => (
+//             <Avatar name={item.username}
+            
+//         ))}
+
+
+//         </Grid>
+//     </Grid>
+//     );
+// };
+
