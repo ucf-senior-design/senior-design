8c90113543e0f20ed2d150bec5bb75920f55b3d3
Jara <araj_mont@yahoo.com>
2023-03-13 10:35:15 -0400

    fixed errors

--- components/Dashboard/Widgets/Poll/PollResult.tsx
+++ components/Dashboard/Widgets/Poll/PollResult.tsx
@@ -1,23 +1,49 @@
 import { Box, Grid, LinearProgress, Typography } from "@mui/material"
-import { PollOption as PollOptionType } from "../../../../utility/types/trip"
-function OptionResults({ option }: { option: PollOptionType }) {
-  // TODO:  Helper function to store percentage */}
+import React from "react"
+import theme from "../../../../styles/theme/Theme"
+import usePoll from "../../../../utility/hooks/polls"
+import { Poll as PollType, PollOption as PollOptionType } from "../../../../utility/types/trip"
+
+function OptionResults({
+  option,
+  pollWidget,
+  value,
+}: {
+  option: PollOptionType
+  pollWidget: PollType
+  value: number
+}) {
+  const { getIndex, poll } = usePoll(pollWidget)
+
   return (
     <LinearProgress
       sx={{ borderRadius: "2px", height: "10px" }}
       variant="determinate"
-      color="secondary"
-      value={50}
+      color={
+        getIndex(option.value) === poll.vote ? (theme.palette.highlight.main as any) : "secondary"
+      } // highlights the option if the user voted for it
+      value={value}
     />
   )
 }
 
-export default function PollResult({ options }: { options: Array<PollOptionType> }) {
+export default function PollResult({
+  options,
+  pollWidget,
+}: {
+  options: Array<PollOptionType>
+  pollWidget: PollType
+}) {
+  const { pollResults, getIndex } = usePoll(pollWidget)
+  const [percent, setPercent] = React.useState(0)
+
+
   return (
     <Box sx={{ display: "flex", flexDirection: "column", gap: 2 }}>
       <>
-        {/** TODO: Handle whether the option is selected or not. */}
         {options.map((option, index) => {
+          setPercent(pollResults(getIndex(option.value)))
+
           return (
             <>
               <Grid container>
@@ -38,12 +64,11 @@ export default function PollResult({ options }: { options: Array<PollOptionType>
                       fontSize: "12px",
                     }}
                   >
-                    {/* TODO: Helper function to store percentage */}
-                    55%
+                    {percent}%
                   </Typography>
                 </Grid>
               </Grid>
-              <OptionResults key={index} option={option} />
+              <OptionResults key={index} option={option} pollWidget={pollWidget} value={percent} />
             </>
           )
         })}
