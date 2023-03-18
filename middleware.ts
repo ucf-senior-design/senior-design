f8cfc8ca330732afa859566055dd4b57b9f98120
Victoria Williamson <vicwill01@knights.ucf.edu>
2023-03-18 19:58:43 -0400

    put file in right location

---
+++ middleware.ts
@@ -0,0 +1,29 @@
+import {NextRequest, NextResponse} from "next/server";
+
+export function middleware(request: NextRequest) {
+  // Clone the request headers and set a new header `x-hello-from-middleware1`
+  const requestHeaders = new Headers(request.headers);
+  requestHeaders.set(
+    "Access-Control-Allow-Origin",
+    "https://we-tinerary-ucf-senior-design.vercel.app/"
+  );
+  requestHeaders.set(
+    "Access-Control-Allow-Methods",
+    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
+  );
+  requestHeaders.set(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept"
+  );
+  requestHeaders.set("Access-Control-Allow-Credentials", "true");
+
+  // You can also set request headers in NextResponse.rewrite
+  const response = NextResponse.next({
+    // @ts-ignore
+    request: {
+      headers: requestHeaders,
+    },
+  });
+
+  return response;
+}
