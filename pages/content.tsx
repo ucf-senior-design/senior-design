a5002bb6e7d6569ed0136a80ff980ee8c664bb3f
Victoria Williamson <vicwill01@knights.ucf.edu>
2023-03-19 13:01:34 -0400

    remove old page

--- pages/content.tsx
+++
@@ -1,9 +0,0 @@
-import { DashboardContentProvder } from "../utility/hooks/dashboardContent"
-
-export default function Content() {
-  return (
-    <DashboardContentProvder>
-      <></>
-    </DashboardContentProvder>
-  )
-}
