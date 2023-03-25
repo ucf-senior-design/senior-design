ace03098191ca4e78062d4e4aaa382e8a35f9557
Victoria Williamson <vicwill01@knights.ucf.edu>
2023-03-25 17:09:49 -0400

    handle testing

--- __tests__/pages/api/auth/sampleTest.test.ts
+++ __tests__/pages/api/auth/sampleTest.test.ts
@@ -1,17 +1,17 @@
-import handler from "../../../../pages/api/auth/loginWithCred"
+import handler from "../../../../pages/api/testendpoint"
 import { testHandler } from "../../../../__mocks__/fetch"
 
-describe("Test Login Endpoints", () => {
+describe("Test Sample Endpoints", () => {
   beforeEach(() => {
     jest.resetAllMocks()
   })
-  test("[Status 400]: Invalid Provider", async () => {
+  test("[Status 200]: Success", async () => {
     const res = await testHandler(handler, {
       body: {
         idToken: "",
         provider: "github",
       },
     })
-    expect(res.statusCode).toBe(400)
+    expect(res.statusCode).toBe(200)
   })
 })
