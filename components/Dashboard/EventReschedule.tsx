8592e0cde876321c154f034b8bf634844ba8da34
farnsworthe <farnsworthe@knights.ucf.edu>
2023-04-05 14:36:23 -0400

    update event reschedule

--- components/Dashboard/EventReschedule.tsx
+++ components/Dashboard/EventReschedule.tsx
@@ -1,24 +1,18 @@
 import { Button, Paper, Typography } from "@mui/material"
 import { DatePicker } from "antd"
 
-export function EventReschedule() {
+export default function EventReschedule({ event }: { event: Event }) {
   const { RangePicker } = DatePicker
 
   return (
     <>
       <Paper sx={{ padding: "20px", width: "80vw", maxWidth: "400px" }}>
-        <Typography sx={{ fontSize: "20px", fontWeight: "600", textAlign: "center" }}>
+        <Typography sx={{ fontSize: "20px", fontWeight: "600", textAlign: "center", padding:"3 em" }}>
           Reschedule Event
         </Typography>
-        <br />
-        Please select start and end date and time to re-schedule the selected event to:
-        <br />
-        <br />
-        <RangePicker showTime />
-        <br />
-        <br />
-        <Button variant="outlined">submit</Button>
-        <br />
+        <p>Please select start and end date and time to re-schedule the selected event to:</p>
+        <p><RangePicker showTime /></p>
+        <p><Button variant="outlined">submit</Button></p>
       </Paper>
     </>
   )
