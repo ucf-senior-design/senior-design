cbc9efe3d346a8c42ef2d56376c6376cb744d4c1
Victoria Williamson <vicwill01@knights.ucf.edu>
2023-04-09 15:52:17 -0400

    calendar updates

--- utility/hooks/availabillity.tsx
+++ utility/hooks/availabillity.tsx
@@ -1,3 +1,4 @@
+import dayjs from "dayjs"
 import React from "react"
 import { API_URL } from "../constants"
 import { createFetchRequestOptions } from "../fetch"
@@ -6,13 +7,36 @@ import { useAuth } from "./authentication"
 import { useScreen } from "./screen"
 import { useTrip } from "./trip"
 
-export default function Availabillity(a: Availabillity) {
+export default function useAvailabillity(a: Availabillity) {
   const { trip } = useTrip()
   const { user } = useAuth()
   const { updateErrorToast } = useScreen()
   const [availabillity, setAvailabillity] = React.useState(a)
+  const [dates, setDates] = React.useState<Map<string, Array<string>>>()
   const BASE_ERROR = "try again later"
 
+  function createDateHash(month: number, day: number) {
+    return `${month}:${day}`
+  }
+  function storeDates() {
+    let dateMap = new Map<string, Array<string>>()
+
+    availabillity.availabillities.forEach((a, user) => {
+      a.dates.forEach((duration) => {
+        for (
+          let current = dayjs(duration.start);
+          current <= dayjs(duration.end);
+          current.add(1, "day")
+        ) {
+          let arrayOfUsers = dateMap.get(createDateHash(current.month(), current.day())) ?? []
+          arrayOfUsers.push(user)
+          dateMap.set(createDateHash(current.month(), current.day()), arrayOfUsers)
+        }
+      })
+    })
+
+    setDates(dateMap)
+  }
   async function updateAvailabillity(dates: Array<Duration>) {
     if (user === undefined || availabillity.availabillities.get(user.uid) === undefined) {
       updateErrorToast(BASE_ERROR)
@@ -35,5 +59,5 @@ export default function Availabillity(a: Availabillity) {
     )
   }
 
-  return { updateAvailabillity }
+  return { updateAvailabillity, dates }
 }
